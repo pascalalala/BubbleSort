d04f6075b1c3cd1fe075336f29eee0bed8ccf360
Pascal <pascal.reinhardt@gmx.net>
2023-04-26 16:50:11 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] array = {1, 3, 6, 4, 9, 7, 8, 2, 5};
+
+        int[] b = sort(array);
+
+        for(int i= 0; i< b.length; i++){
+            System.out.println(b[i]);
+        }
+
+
+    }
+
+
+        public static int[] sort(int[] a){
+            int temp = 0;
+            for (int i = 0; i < a.length; i++) {
+                for (int j = i+1; j < a.length; j++) {
+                      if(a[i] > a[j]){
+                          temp = a[i];
+                          a[i] = a[j];
+                          a[j] = temp;
+                      }
+
+                }
+            }return a;
+        }
+
+
+
+
+    }
